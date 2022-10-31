c4ce0124753e23b63a139b81a93a37e357ace7ee
Jnissan2 <Jacobnissan@ufl.edu>
2022-10-31 16:05:44 -0400

    Made it subtract

--- labsix.py
+++ labsix.py
@@ -1,8 +1,8 @@
 if __name__ == '__main__':
-    print("Pick 2 numbers to add")
+    print("Pick 2 numbers to subtract")
     print("Number 1: ")
     option1 = input()
     print("Number 2: ")
     option2 = input()
-    number=int(option1)+int(option2)
+    number=int(option1)-int(option2)
     print("Your number is " + str(number))
