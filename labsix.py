2de64ca7adfdfe7548056dca5ba61e1747cd071a
Jnissan2 <Jacobnissan@ufl.edu>
2022-10-31 15:48:09 -0400

    Jaocb is uploading first file

---
+++ labsix.py
@@ -0,0 +1,8 @@
+if __name__ == '__main__':
+    print("Pick 2 numbers to add")
+    print("Number 1: ")
+    option1 = input()
+    print("Number 2: ")
+    option2 = input()
+    number=int(option1)+int(option2)
+    print("Your number is " + str(number))
